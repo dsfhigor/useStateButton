9888df2f459ae831f057aa1b6f069a07d9908928
Hígor da Silva Flores <dsfhigor@gmail.com>
2025-02-02 12:30:46 -0300

    useStatePage

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Contadores atualizados separadamente.
+        </h1>
+        <MyButton />
+        <MyButton />
+
+      </header>
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleCLick() {
+    setCount(count + 1);
+  }
+  return (
+    <button onClick={handleCLick}>Clicou {count} vezes</button>
+  );
+}
+export default App;
